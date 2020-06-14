bdedb4c50ccd51da0d8f8c9a4c3807dee6db5529
bigbro0991 <bigbro0991@gmail.com>
2020-06-13 21:16:49 -0400

    half project

--- Instagram/urls.py
+++ Instagram/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include,path
-
+from Insta.views import SignUpView
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Instagram/', include('Insta.urls'))
+    path('Instagram/', include('Insta.urls')),
+    path('auth/', include('django.contrib.auth.urls')),
+    path('auth/signup',SignUpView.as_view(), name="signup"),
 ]
