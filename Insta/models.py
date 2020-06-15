c655a35c68043b25764f100403158c4cc6091620
bigbro0991 <bigbro0991@gmail.com>
2020-06-14 21:20:05 -0400

    add likes models

--- Insta/models.py
+++ Insta/models.py
@@ -3,7 +3,23 @@ from imagekit.models import ProcessedImageField
 from django.urls import reverse
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class InstaUser(AbstractUser):
+    profile_pic=ProcessedImageField(
+        upload_to='static/images/profile',
+        format='JPEG',
+        options={'quality':100},
+        blank=True,
+        null=True
+    )
+
+
 class Post(models.Model):
+    author = models.ForeignKey(
+        InstaUser,
+        on_delete=models.CASCADE,
+        related_name="my_posts"
+    )
     title = models.TextField(blank=True, null=True)
     image = ProcessedImageField(
         upload_to='static/images/posts',
@@ -14,14 +30,21 @@ class Post(models.Model):
     )
 
     def get_absolute_url(self):
-        return reverse("posts_detail",args=[str(self.id)])
+        return reverse("post_detail",args=[str(self.pk)])
 
 
-class InstaUser(AbstractUser):
-    profile_pic=ProcessedImageField(
-        upload_to='static/images/profile',
-        format='JPEG',
-        options={'quality':100},
-        blank=True,
-        null=True
-    )
+class Like(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="likes")
+    user = models.ForeignKey(
+        InstaUser,
+        on_delete=models.CASCADE,
+        related_name="likes")
+    
+    class Meta:##distinct in sql
+        unique_together = ("post","user")
+
+    def __str__(self):
+        return "like"+str(self.pk)+": "+ self.user.username + ' likes ' + self.post.title
--- Insta/urls.py
+++ Insta/urls.py
@@ -21,7 +21,7 @@ from Insta.views import (HelloWorld,PostsListView,PostsDetailView,PostsCreateVie
 urlpatterns = [
     path('', HelloWorld.as_view(),name='helloword'),  
     path('posts/', PostsListView.as_view(),name='posts'),  
-    path('post/<int:pk>/', PostsDetailView.as_view(), name='posts_detail'),
+    path('post/<int:pk>/', PostsDetailView.as_view(), name='post_detail'),
     path('posts/new/', PostsCreateView.as_view(), name='make_post'),
     path('posts/update/<int:pk>', PostsUpdateView.as_view(), name='update_post'),
     path('posts/delete/<int:pk>', PostsDeleteView.as_view(), name='delete_post'),
